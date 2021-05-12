9be5ce2522bd0d0bfa50466e8e0f71df2eb366a9
Ekram70 <ekramullah70@gmail.com>
2021-05-12 14:19:20 +0600

    Some simplification to html and js file

--- main.js
+++ main.js
@@ -1,27 +1,28 @@
+//Declaring the variables.
 let pass = document.getElementsByClassName("password")[0];
 let show = document.getElementsByClassName("show")[0];
 let hide = document.getElementsByClassName("hide")[0];
-let para = document.getElementsByClassName("test")[0];
 let clear = document.getElementsByClassName("clear")[0];
 let submit = document.getElementsByClassName("submit")[0];
 
-show.addEventListener("click", function () {
-  pass.setAttribute("type", "text");
-  let val = pass.value;
-  para.innerHTML = val;
-});
+//Common function for show hide
+function showHide(btn, type) {
+  btn.addEventListener("click", function () {
+    pass.setAttribute("type", type);
+  });
+}
 
-hide.addEventListener("click", function () {
-  pass.setAttribute("type", "password");
-  let val = pass.value;
-  para.innerHTML = val;
-});
+showHide(show, "text");
+showHide(hide, "password");
 
+//clear button js
 clear.addEventListener("click", function () {
   pass.value = "";
 });
 
+//submit button js
 submit.addEventListener("click", function () {
+  //creating an array from each characters of input value.
   let arr = [];
   let char;
   let passLength = pass.value.length;
@@ -29,89 +30,82 @@ submit.addEventListener("click", function () {
     char = pass.value.slice(i, i + 1);
     arr.push(char);
   }
-
   console.log(arr);
 
-  let check=0;
+  //password validation code.
 
-  let count=0;
+  //declaring variables.
+  let check = 0;
+  let count = 0;
+  let i = 0;
 
-  if(passLength>=8){
+  //8 characters condition check.
+  if (passLength >= 8) {
     check++;
-  }
-  else{
+  } else {
     console.log("Password Must be at least Eight Character!");
   }
 
-  for(let i=0;i<passLength;i++)
-  {
-    if(arr[i]>='a' && arr[i]<='z')
-    {
+  //Lowercase letter condition check.
+  for (i = 0; i < passLength; i++) {
+    if (arr[i] >= "a" && arr[i] <= "z") {
       count++;
     }
   }
-  if(count>0)
-  {
+  if (count > 0) {
     check++;
-  }
-  else{
+  } else {
     console.log("Password Must Contain a Lowercase Character!");
   }
-  count=0;
+  count = 0;
 
-
-  for(let i=0;i<passLength;i++)
-  {
-    if(arr[i]>='A' && arr[i]<='Z')
-    {
+  //Uppercase letter condition check.
+  for (i = 0; i < passLength; i++) {
+    if (arr[i] >= "A" && arr[i] <= "Z") {
       count++;
     }
   }
-  if(count>0)
-  {
+  if (count > 0) {
     check++;
-  }
-  else{
+  } else {
     console.log("Password Must Contain a Uppercase Character!");
   }
-  count=0;
+  count = 0;
 
-
-  for(let i=0;i<passLength;i++)
-  {
-    if(arr[i]>='0' && arr[i]<='9')
-    {
+  //Number condition check.
+  for (i = 0; i < passLength; i++) {
+    if (arr[i] >= "0" && arr[i] <= "9") {
       count++;
     }
   }
-  if(count>0)
-  {
+  if (count > 0) {
     check++;
-  }
-  else{
+  } else {
     console.log("Password Must Contain a Number!");
   }
-  count=0;
+  count = 0;
 
-  for(let i=0;i<passLength;i++)
-  {
-    if(!(arr[i]>='0' && arr[i]<='9') && !(arr[i]>='a' && arr[i]<='z') && !(arr[i]>='A' && arr[i]<='Z'))
-    {
+  //Special character condition check.
+  for (i = 0; i < passLength; i++) {
+    if (
+      !(arr[i] >= "0" && arr[i] <= "9") &&
+      !(arr[i] >= "a" && arr[i] <= "z") &&
+      !(arr[i] >= "A" && arr[i] <= "Z")
+    ) {
       count++;
     }
   }
-  if(count>0)
-  {
+  if (count > 0) {
     check++;
-  }
-  else{
+  } else {
     console.log("Password Must Contain a Special Character!");
   }
+  count = 0;
 
-  if(check==5){
+  //password validation
+  if (check == 5) {
     console.log("Password accepted");
-  }
-  else{
+  } else {
     console.log("Password Not Accepted");
   }
 });
