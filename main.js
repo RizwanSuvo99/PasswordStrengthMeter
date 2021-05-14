1fd9531e185cefaabd91abfc83b842f1f9850586
Ekram70 <ekramullah70@gmail.com>
2021-05-14 18:22:31 +0600

    Eight characters message shown

--- main.js
+++ main.js
@@ -20,22 +20,63 @@ pass.addEventListener("input", function () {
   // root.style.setProperty("--display", "block");
 
   // This is for characters
-  let inputVal = pass.value;
-  char.innerHTML = inputVal.length + " characters";
+  let passLength = pass.value.length;
+  char.innerHTML = passLength + " characters";
   // This is for validation bar
-  if (inputVal.length == 0) {
+  if (passLength == 0) {
     bar.style.width = "0%";
-  } else if (inputVal.length > 0 && inputVal.length <= 4) {
+  } else if (passLength > 0 && passLength <= 4) {
     bar.style.width = "25%";
     bar.style.backgroundColor = "red";
-  } else if (inputVal.length > 4 && inputVal.length <= 8) {
+  } else if (passLength > 4 && passLength <= 8) {
     bar.style.width = "50%";
     bar.style.backgroundColor = "orange";
-  } else if (inputVal.length > 9 && inputVal.length <= 12) {
+  } else if (passLength > 9 && passLength <= 12) {
     bar.style.width = "75%";
     bar.style.backgroundColor = "#ffe77aff";
   } else {
     bar.style.width = "100%";
     bar.style.backgroundColor = "#16c7a1";
   }
+
+  // Lowercase, Uppercase, Number, Symbol
+
+  // Creating an array from Password value
+  let arr = [];
+  let character;
+  for (let i = 0; i < passLength; i++) {
+    character = pass.value.slice(i, i + 1);
+    arr.push(character);
+  }
+
+  /*
+  
+  This
+  TAsk 
+  is
+  for 
+  rizwan
+  vai
+
+  */
+
+  // Declaring the variables
+  let check = 0;
+  let count = 0;
+  let i = 0;
+  let j = 0;
+  let eightText = document.getElementById("eight");
+  let condText = document.getElementsByClassName("condition-text");
+
+  //8 characters condition check.
+  if (passLength == 0) {
+    eightText.style.display = "none";
+  } else if (passLength >= 8) {
+    check++;
+    eightText.style.display = "none";
+  } else {
+    eightText.style.display = "block";
+  }
+
+  //common function for Lowercase, Uppercase, Number condition
 });
